fcd133c10da837b830ec869e50bfb649a373bac0
Ximena Rodriguez Rodriguez <ximenarodrod8@gmail.com>
2025-02-05 12:43:05 -0600

    Agregar menu al proyecto

--- src/main/java/com/example/tap2025/HelloApplication.java
+++ src/main/java/com/example/tap2025/HelloApplication.java
@@ -2,22 +2,65 @@ package com.example.tap2025;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private Button btnSaludo, btnSaludo2, btnSaludo3; //covencion camelCase (forma de nombrar los objetos
+
+    private VBox vBox; //herramienta para situar los botones de manera vertical, usar HBox de manera horizontal
+    private MenuBar mnbPrincipal;
+    private Menu menCompetencia1, getMenCompetencia2;
+    private MenuItem mitCalculadora;
+    private Scene scena;
+    void CrerUI(){
+        //ir de lo particular a lo general
+        mitCalculadora= new MenuItem("Calculadora");
+        menCompetencia1=new Menu("Competencia 1");
+        menCompetencia1.getItems().addAll(mitCalculadora);
+        mnbPrincipal=new MenuBar();
+        mnbPrincipal.getMenus().addAll(menCompetencia1);
+
+    }
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+
+        vBox= new VBox();
+        stage.setTitle("Hola Mundo de Eventos <3");
+        stage.setScene(new Scene(vBox, 200,200));
         stage.show();
+        stage.setMaximized(true); //para maximizar la ventana
     }
 
     public static void main(String[] args) {
         launch();
     }
+
 }
+/*
+//se instancia de lo oparticular o a lo general
+//ya que vBox, por ejemplo, tiene la opcion de instanciar los opjetos ya instancias anteriormente
+btnSaludo=new Button("Bienvenido madafacker <3");
+btnSaludo.setOnAction(actionEvent ->clickEvent("Hola"));//estructura lamda
+btnSaludo2=new Button("Bienvenido madafacker <3");
+btnSaludo2.setOnAction(actionEvent ->clickEvent("Mundo"));//estructura lamda
+btnSaludo3=new Button("Bienvenido madafacker <3");
+btnSaludo3.setOnAction(actionEvent ->clickEvent("<3"));//estructura lamda
+vBox.setSpacing(10);//setSpacing para espaciar, el double se refiere a pixeles
+//padding es respecto del padre hacia el hijo, y cuando se habla del margen es desde el hijo, respecto al padre
+//padding horizontal
+//margin vertical
+vBox.setPadding(new Insets(10, 0,0,0));//insets es una clase mas, por ello se instancia
+void clickEvent(String mensaje){
+    System.out.println(mensaje);
+}
+private Button btnSaludo, btnSaludo2, btnSaludo3; //covencion camelCase (forma de nombrar los objetos
+*/
